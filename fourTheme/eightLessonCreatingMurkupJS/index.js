7624dbaf5e9e9368e6d999b43862de4681e8664f
Mikhail Monakhov <mixa.monakh@gmail.com>
2023-04-27 13:37:15 +0300

    настраивал вебпак

--- fourTheme/eightLessonCreatingMurkupJS/index.js
+++ fourTheme/eightLessonCreatingMurkupJS/index.js
@@ -1,5 +1,54 @@
 // Восьмой урок: Создание разметки HTML при помощи методов JS
 
-var a = getElementById('#fff')
+// Функция принимает объект
+// function render( obj ){
+//     return newNode;
+// }
 
-console.log(a);
+// // Передаваемый в функцию объект имеет следующий вид:
+
+// var test = {
+//     // Ключ имя тега
+//     tagName: 'input', // Обязателньый ключ
+//     // Ключ атрибуты
+//     attrs: [
+//         {
+//             name: 'type',
+//             value: 'text'
+//         },
+//         {
+//             name: 'placehloder',
+//             value: 'Name'
+//         }
+//     ],
+//     // Ключ классы
+//     className: ['class1', 'class2'],
+//     props:[
+//         {
+//             name: 'required',
+//             value: true
+//         }
+//     ],
+//     children: [ ...DomNodes ]
+// }
+
+// Мы вызываем фунцию и передаем в нее объект. Вызов функции присваиваем в переменную что бы потом можно было создать разметку
+
+// var newDiv = render({
+//     tagName: 'div',
+//     className: ['div', 'div-2']
+// })
+
+// document.body.appendChild( newDiv );
+
+
+
+// Отработка моментов из урока:
+
+//Повесить сообщение об отправки на форму
+
+var forma = document.querySelector('.form');
+
+forma.addEventListener( 'submit', function(){
+    alert('Send!!');
+});
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,19 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     entry: './index.js',
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'bundle.js'
-      },
+    },
+    plugins: [new HtmlWebpackPlugin()],
+    devServer: {
+        static: './dist/',
+        hot: false,
+        devMiddleware: {
+            publicPath: '/dist/',
+            writeToDisk: true,
+         },    
+       }
   };
